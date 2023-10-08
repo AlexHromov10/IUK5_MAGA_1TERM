c3232af9efee413f55aaab4db75cf98578a3d610
AlexHromov10 <alexhromov228@gmail.com>
2023-10-09 02:38:34 +0300

    finished sm (maybe)

--- sm_lab_1_src/index.py
+++ sm_lab_1_src/index.py
@@ -3,18 +3,24 @@ import tkinter as tk
 from PIL import Image, ImageEnhance, ImageTk
 
 
+# Обновление общего контраста
 def update_contrast(value):
     global cyan_scale, magenta_scale, yellow_scale, img, img_tk
 
+    # Сброс значений контраста по CMYK
     cyan_scale.set(1)
     magenta_scale.set(1)
     yellow_scale.set(1)
 
+    # Изменение общего контраста
     enhancer = ImageEnhance.Contrast(img)
     enhanced_img = enhancer.enhance(float(value))
 
+    # Рендер изображение
     img_tk = ImageTk.PhotoImage(enhanced_img)
     image_label.configure(image=img_tk)
+
+# Изменение значения контраста для цвета Cyan
 
 
 def update_cyan(value):
@@ -22,11 +28,15 @@ def update_cyan(value):
     c = value
     update_cmyk(c, m, y)
 
+# Изменение значения контраста для цвета Magenta
+
 
 def update_magenta(value):
     global c, m, y
     m = value
     update_cmyk(c, m, y)
+
+# Изменение значения контраста для цвета Yellow
 
 
 def update_yellow(value):
@@ -34,16 +44,22 @@ def update_yellow(value):
     y = value
     update_cmyk(c, m, y)
 
+# Изменение контраста по CMYK
+
 
 def update_cmyk(c, m, y):
     global img, img_tk, contrast_scale
 
+    # Сброс значений общего контраста
     contrast_scale.set(1)
 
+    # Вывод для debug
     print(float(c), float(m), float(y))
 
+    # Разделение изображения по CMYK каналам
     c_img, m_img, y_img, k_img = img.split()
 
+    # Контраст по цветам
     c_enhancer = ImageEnhance.Contrast(c_img)
     m_enhancer = ImageEnhance.Contrast(m_img)
     y_enhancer = ImageEnhance.Contrast(y_img)
@@ -52,11 +68,15 @@ def update_cmyk(c, m, y):
     m_enhanced_img = m_enhancer.enhance(float(m))
     y_enhanced_img = y_enhancer.enhance(float(y))
 
+    # Слияние измененных каналов
     final_img = Image.merge("CMYK", [c_enhanced_img,
                             m_enhanced_img, y_enhanced_img, k_img])
 
+    # Рендер изображение
     img_tk = ImageTk.PhotoImage(final_img)
     image_label.configure(image=img_tk)
+
+# Сброс всех значений
 
 
 def reset_scales():
@@ -67,25 +87,34 @@ def reset_scales():
     yellow_scale.set(1)
 
 
+# Глобальные значения цветов CMYK
 c = 1
 m = 1
 y = 1
 
+# Значения для слайдера
 set_from = 0
 set_to = 2.0
+
+# Значение по умолчанию
 set_default = 1.0
 
+# root окна
 root = tk.Tk()
+
+# Название окна
 root.title("Contrast")
 
-# Load and display the original image
+# Загрузка изображения
 img = Image.open("image.jpg").convert('CMYK')
 img_tk = ImageTk.PhotoImage(img)
+
+# Конфигурирование элемента изображения
 image_label = tk.Label(root, image=img_tk, height=300, width=300)
 image_label.grid(row=1, column=1, columnspan=4)
 
-# Contrast slider
-contrast_label = tk.Label(root, text="Контрастность")
+# Слайдер контраста
+contrast_label = tk.Label(root, text="Контраст")
 contrast_label.grid(row=2, column=1)
 
 contrast_scale = tk.Scale(root, from_=0, to=2.0, resolution=0.1,
@@ -93,11 +122,11 @@ contrast_scale = tk.Scale(root, from_=0, to=2.0, resolution=0.1,
 contrast_scale.set(1.0)
 contrast_scale.grid(row=2, column=2)
 
-# Reset button
-reset_button = tk.Button(root, text="Reset", command=reset_scales)
+# Кнопка сброса
+reset_button = tk.Button(root, text="Сбросить", command=reset_scales)
 reset_button.grid(row=3, column=1)
 
-# Cyan slider
+# Слайдер cyan
 cyan_label = tk.Label(root, text="C")
 cyan_label.grid(row=2, column=3)
 
@@ -106,7 +135,7 @@ cyan_scale = tk.Scale(root, from_=set_from, to=set_to, resolution=0.1,
 cyan_scale.set(set_default)
 cyan_scale.grid(row=2, column=4)
 
-# Magenta slider
+# Слайдер magenta
 magenta_label = tk.Label(root, text="M")
 magenta_label.grid(row=3, column=3)
 
@@ -115,7 +144,7 @@ magenta_scale = tk.Scale(root, from_=set_from, to=set_to, resolution=0.1,
 magenta_scale.set(set_default)
 magenta_scale.grid(row=3, column=4)
 
-# Yellow slider
+# Слайдер yellow
 yellow_label = tk.Label(root, text="Y")
 yellow_label.grid(row=4, column=3)
 
@@ -124,4 +153,5 @@ yellow_scale = tk.Scale(root, from_=set_from, to=set_to, resolution=0.1,
 yellow_scale.set(set_default)
 yellow_scale.grid(row=4, column=4)
 
+# Цикл окна
 root.mainloop()
